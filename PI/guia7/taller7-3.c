cb53ef28bf293e635fd40be9d735723ab710243c
jglaubart <jonasglaubart@gmail.com>
2024-05-06 09:20:26 -0300

    guia7

--- PI/guia7/taller7-3.c
+++ PI/guia7/taller7-3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "getnum.h"
-#include "bibliorandom.h"
+#include <time.h>
 #define RANGOMOV 2
+#define CUADRADO 5
 typedef struct {
 int x;
 int y;
@@ -10,7 +11,7 @@ int y;
 
 
 int main(){
-    randomize();
+    srand(time(NULL));
     int bloque = 10;
     tPunto2D *posiciones = malloc(bloque*sizeof(tPunto2D));  //agrego memoria cuando llega al maximo
     tPunto2D lugar;
@@ -19,29 +20,28 @@ int main(){
     int dim=0, dx, dy;
     printf("Empieza en el (%d , %d)\n", lugar.x, lugar.y);
     // el primer cambio lo hago fuera y despues el while es hasta que sea 0,0 de nuevo
-    dx = randInt(-RANGOMOV,RANGOMOV);
-    dy = randInt(-RANGOMOV,RANGOMOV);
+    dx = rand() % (RANGOMOV - (-RANGOMOV) + 1) -RANGOMOV;
+    dy = rand() % (RANGOMOV - (-RANGOMOV) + 1) -RANGOMOV;
     lugar .x += dx;
     lugar .y += dy;
     printf("(%d , %d) en movimiento %d\n", lugar.x, lugar.y, dim+1);
 
     posiciones[dim] = lugar;    //guardo la struct lugar con los puntos en el vector
-    char llegoAlCero = (lugar .x == 0 && lugar .y == 0); 
+    char llegoAlCero = 0; 
     while (!llegoAlCero){
-        dx = randInt(-RANGOMOV,RANGOMOV);
-        dy = randInt(-RANGOMOV,RANGOMOV);
-        if( (lugar.x + dx) <= 10 && (lugar.y + dy) <= 10){  // hago un cuadrado de 10x10 por el que se puede mover
+        dx = rand() % (RANGOMOV - (-RANGOMOV) + 1) -RANGOMOV;
+        dy = rand() % (RANGOMOV - (-RANGOMOV) + 1) -RANGOMOV;
+        if( (lugar.x + dx) <= CUADRADO && (lugar.y + dy) <= CUADRADO && (lugar.x + dx) >= -CUADRADO && (lugar.y + dy) >= -CUADRADO){  // hago un cuadrado de 5X5 por el que se puede mover
             lugar .x += dx;
             lugar .y += dy;
             posiciones[dim++] = lugar; 
-            //printf("(%d , %d) en movimiento %d\n", lugar.x, lugar.y, dim+1); 
-            llegoAlCero = (lugar .x == 0 && lugar .y == 0);
+            printf("(%d , %d) en movimiento %d\n", lugar.x, lugar.y, dim); 
+            llegoAlCero = ( (lugar .x) == 0 && (lugar .y) == 0);
             if (dim % bloque == 0 && !llegoAlCero){
                 // aumento memoria
                 posiciones = realloc(posiciones, (dim + bloque)*sizeof(tPunto2D));
             }
         }
-        //if(dim==9) {llegoAlCero=1;}
     }
     posiciones = realloc(posiciones, (dim)*sizeof(tPunto2D)); //saco la menoria sobrante
     free (posiciones);
